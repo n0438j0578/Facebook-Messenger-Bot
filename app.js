b8e3037f694ae780fb41d015d23624d24d6861c8
notnuttt <nutnutnut12079@hotmail.com>
2018-09-18 16:12:21 +0700

    add random

--- app.js
+++ app.js
@@ -377,7 +377,7 @@ function receivedMessage(event) {
         dbo.collection("word").findOne({}, function(err, result) {
           if (err) throw err;
           //console.log(result.Hi);
-          sendTextMessage(senderID, result.Hi[0]);
+          sendTextMessage(senderID, result.Hi[Math.random() * 6]);
           db.close();
         });
       }); 
