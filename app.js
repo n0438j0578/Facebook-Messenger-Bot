393c7dd91ba1785cdd52097f7bdb91a10f6b69fd
pongsakon <pongsakonpromma@gmail.com>
2018-09-18 09:56:09 +0700

    ลองต่อเดต้าเบสอีกครั้ง

--- app.js
+++ app.js
@@ -266,14 +266,14 @@ function receivedMessage(event) {
     //   }).catch(function (err) {
     //     // test = "แงงงซวยยย";
     //   })
-        //var url = "mongodb://<test>:<joenut1234>@ds046677.mlab.com:46677/chatbot";
+        var url = "mongodb://test:joenut1234@ds046677.mlab.com:46677/chatbot";
 
-        // MongoClient.connect(url, function(err, db) {
-        //   if (err) throw err;
-        //   console.log("Database created!");
-        //   sendTextMessage(senderID, "ต่อเข้าชิมิ");
-        //   db.close();
-        // });
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          console.log("Database created!");
+          sendTextMessage(senderID, "ต่อเข้าชิมิ");
+          db.close();
+        });
 
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
