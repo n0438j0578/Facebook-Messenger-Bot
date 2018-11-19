bf70997119e967274d8309d5c96ad46bf10f2ccc
pongsakon <pongsakonpromma@gmail.com>
2018-11-19 23:50:18 +0700

    เพิ่มตัวอย่างการเทส

--- test.js
+++ test.js
@@ -16,6 +16,43 @@
 //     }
 // );
 
+
+// //เทสกรณีถามว่าของเหลือไหม
+
+// var request = require('request');
+// request.post(
+//     'http://35.240.139.71:20000/api/word ',
+//     { json: {
+//         "text":"ASUS Dual-band Wireless-AC1200 router RT-AC1200G+ เหลือไหม"
+//     } },
+//     function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body)
+
+//             //console.log(body.Answer.Img)
+//         }
+//     }
+// );
+
+
+// //เทสกรณีที่ไม่มีคำตอบ
+
+// var request = require('request');
+// request.post(
+//     'http://35.240.139.71:20000/api/word ',
+//     { json: {
+//         "text":"มีหน้าร้านไหม"
+//     } },
+//     function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body)
+
+//             //console.log(body.Answer.Img)
+//         }
+//     }
+// );
+
+
 // //เทสกระบวนเตรียมคำและให้ผู้ใข้เลือกต่อว่าคำไหนไม่มีความจำเป็น
 
 // var request = require('request');
