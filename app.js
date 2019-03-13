867b123d30bb846b59c625e78227ecd4415a797d
pongsakon <pongsakonpromma@gmail.com>
2019-03-13 15:58:34 +0700

    แก้อะไรนิดหน่อย

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const
   express = require('express'),
   https = require('https'),  
   request = require('request');
+  cors = require('cors')
 
 var app = express();
+app.use(cors())
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
